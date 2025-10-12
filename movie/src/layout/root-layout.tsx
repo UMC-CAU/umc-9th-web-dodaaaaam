14fb4aa6298e4a57ad297dc39f98b0a3a8eb9928
hyerin <rin4219@naver.com>
2025-10-12 21:20:16 +0900

    로그인 회원가입 페이지 구현 + useForm 적용

--- movie/src/layout/root-layout.tsx
+++ movie/src/layout/root-layout.tsx
@@ -1,10 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import Navbar from '../components/navbar';
 
 const RootLayout = () => {
   return (
     <div className="bg-black text-white">
-      <Navbar />
+      <header className="flex items-center justify-between px-6 py-4">
+        <Navbar />
+        <nav className="flex gap-6 p-4">
+          <Link to="/login" className="hover:text-gray-300">
+            로그인
+          </Link>
+          <Link to="/signUp" className="hover:text-gray-300">
+            회원가입
+          </Link>
+        </nav>
+      </header>
       <Outlet />
     </div>
   );
